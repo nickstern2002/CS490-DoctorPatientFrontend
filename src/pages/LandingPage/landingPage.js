01eaeb6f20052fe3a90649fc361c284488b48b68
Michael Marut <michaelmmarut@gmail.com>
2025-05-01 18:01:03 -0400

    Implement meal selection and calculated calorie tracking

--- src/pages/LandingPage/landingPage.js
+++ src/pages/LandingPage/landingPage.js
@@ -10,47 +10,49 @@ import Footer from '../../Components/Footer/Footer';
 import './LandingPage.css'
 
 
-
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
 function LandingPage() {
   return (
       <>
-        <Navbar />
+        <div className={`global-root ${isDarkMode ? 'dark-mode' : ''}`}>
+          <Navbar />
 
-        {/* Main grey background area */}
-        <div className="landing-background">
+          {/* Main grey background area */}
+          <div className="landing-background">
 
-          <section id="home">
-            <div className="landing-container">
-              <HeroSection />
-            </div>
-          </section>
+            <section id="home">
+              <div className="landing-container">
+                <HeroSection />
+              </div>
+            </section>
 
-          <section id="doctors">
-            <div className="landing-container">
-              <Doctors />
-            </div>
-          </section>
+            <section id="doctors">
+              <div className="landing-container">
+                <Doctors />
+              </div>
+            </section>
 
-          <section id="pharmacists">
-            <div className="landing-container">
-              <Pharmacists />
-            </div>
-          </section>
+            <section id="pharmacists">
+              <div className="landing-container">
+                <Pharmacists />
+              </div>
+            </section>
 
-          <section id="testimonials">
-            <div className="landing-container">
-              <Testimonials />
-            </div>
-          </section>
+            <section id="testimonials">
+              <div className="landing-container">
+                <Testimonials />
+              </div>
+            </section>
 
-          <section id="about">
-            <div className="landing-container">
-              <About />
-            </div>
-          </section>
+            <section id="about">
+              <div className="landing-container">
+                <About />
+              </div>
+            </section>
 
+          </div>
+        <Footer />
         </div>
-      <Footer />
     </>
   );
 }
